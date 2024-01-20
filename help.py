82854be8abc83f3b2f32d76b9c9679c00f017a95
jm-world <unhoiylaw@naver.com>
2024-01-20 21:37:02 +0900

    api urlchanged

--- help.py
+++ help.py
@@ -8,3 +8,8 @@
 # 모델(models.py) 변경 시 alembic 명령어
 # alembic revision --autogenerate
 # alembic upgrade head
+
+# 서버 접속
+# ssh -i ~/myapi.pem ubuntu@15.165.82.55
+# 이후 가상환경 진입
+# ubuntu@jm-world:~/venvs/myapi/bin$ . activate
