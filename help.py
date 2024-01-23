d2256c45ecdcf6669540461f3fc319e9c97b49ec
jm-world <unhoiylaw@naver.com>
2024-01-23 18:25:28 +0900

    주석 추가

--- help.py
+++ help.py
@@ -10,7 +10,7 @@
 # alembic upgrade head
 
 # 서버 접속
-# ssh -i ~/myapi.pem ubuntu@15.165.82.55
+################ ssh -i ~/myapi.pem ubuntu@15.165.82.55
 # 이후 가상환경 진입
 # myapi
 
@@ -18,10 +18,10 @@
 # uvicorn main:app --reload --host=0.0.0.0
 
 # (안씀) gunicorn 시작/ 종료: stop/ 재시작: restart
-# sudo systemctl start myapi.service
+############### sudo systemctl start myapi.service
 
 # nginx 시작: 8000 필요없음
-# sudo systemctl start nginx
+############### sudo systemctl start nginx
 # sudo nano /etc/nginx/sites-available/myapi 명령으로 수정할 수 있다.
 # 서버 로그 확인하기
 # tail -f myapi.log
